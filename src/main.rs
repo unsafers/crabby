b1cd3705528a3b29ecbdfca68c0acbc50b96f9b6
Ing <ingridrosselis@gmail.com>
2021-10-25 03:35:36 +0000

    Add initial code from example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
-fn main() {
-    println!("Hello, world!");
+use rspotify::{
+    model::{AdditionalType, Country, Market},
+    prelude::*,
+    scopes, AuthCodeSpotify, Credentials, OAuth,
+};
+
+#[tokio::main]
+async fn main() {
+    // Set RSPOTIFY_CLIENT_ID and RSPOTIFY_CLIENT_SECRET in an .env file or
+    // export them manually:
+    //
+    // export RSPOTIFY_CLIENT_ID="your client_id"
+    // export RSPOTIFY_CLIENT_SECRET="secret"
+    //
+    // These will then be read with `from_env`.
+    //
+    // Otherwise, set client_id and client_secret explictly:
+    //
+    // ```
+    // let creds = Credentials::new("my-client-id", "my-client-secret");
+    // ```
+    let creds = Credentials::from_env().unwrap();
+
+    // Same for RSPOTIFY_REDIRECT_URI. You can also set it explictly:
+    //
+    // ```
+    // let oauth = OAuth {
+    //     redirect_uri: "http://localhost:8888/callback".to_string(),
+    //     scopes: scopes!("user-read-recently-played"),
+    //     ..Default::default(),
+    // };
+    // ```
+    let oauth = OAuth::from_env(scopes!("user-read-currently-playing")).unwrap();
+
+    let mut spotify = AuthCodeSpotify::new(creds, oauth);
+
+    // Obtaining the access token
+    let url = spotify.get_authorize_url(false).unwrap();
+    spotify.prompt_for_token(&url).await.unwrap();
+
+    // Running the requests
+    let market = Market::Country(Country::Spain);
+    let additional_types = [AdditionalType::Episode];
+    let artists = spotify
+        .current_playing(Some(&market), Some(&additional_types))
+        .await;
+
+    println!("Response: {:?}", artists);
 }
