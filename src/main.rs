d93c42e4deb63084efb34cdf92c735c9a1d4412a
Ing <ingridrosselis@gmail.com>
2022-03-11 21:39:39 -0300

    Explore CLI creation with clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::{Parser, Subcommand};
+
+/// Hello, this is Crabby, your friend that plays Spotify music for you! 
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[clap(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Status,
+    Play,
+    Pause,
+    Stop,
+    Connect,
+    Suffle
+}
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
 }
